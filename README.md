83cc0483f334133a745fe5f00aebd1f192eaf055
Jean Centeno <jeancenteno54@gmail.com>
2023-08-14 10:50:48 -0300

    se agrego URL para editar contacto

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # node-project
+
 Creacion Basica de unas APIs que permita obtener, agregar y borrar contactos utilizando node y express.
 Clonar repositorio
 Instalar las dependencias (npm install)
@@ -12,6 +13,9 @@ Obtener contacto por id:
 
 Agregar contacto:
 /api/contacts
+
+Editar contacto:
+/api/contacts/:id
 
 Eliminar contacto:
 /api/contacts/:id
--- index.mjs
+++ index.mjs
@@ -45,6 +45,27 @@ app.post("/api/contacts", (req, res) => {
   res.send(contact);
 });
 
+app.put("/api/contacts/:id", (req, res) => {
+  const contactId = parseInt(req.params.id);
+  const contactIndex = contacts.findIndex((c) => c.id === contactId);
+
+  if (contactIndex === -1) {
+    return res.status(404).send("Contacto no encontrado");
+  }
+
+  const updatedContact = {
+    id: contactId,
+    nome: req.body.nome,
+    tlf: parseInt(req.body.tlf),
+    email: req.body.email,
+    endereco: req.body.endereco,
+    gender: req.body.gender,
+  };
+
+  contacts[contactIndex] = updatedContact;
+  res.send(updatedContact);
+});
+
 app.delete("/api/contacts/:id", (req, res) => {
   const contact = contacts.find((c) => c.id === parseInt(req.params.id));
   if (!contact) return res.status(404).send("Contacto no encontrado");
